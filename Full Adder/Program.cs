3e1fa5978e9e6befdbe5b65edb68ff1c9c4deec6
Jeroen Egelmeers <mail@jeroenegelmeers.nl>
2015-05-30 00:14:37 +0200

    Psuedo uitleg stappenplan met inplementatie 3 DP's

--- Full Adder/Program.cs
+++ Full Adder/Program.cs
@@ -16,3 +16,14 @@ namespace Full_Adder
         }
     }
 }
+
+/*
+ * == Stap 1: File wordt uitgelezen.
+ * == Stap 2: Uit de file wordt per node een node aangemaakt d.m.v. de factory en aan een list toegevoegd met Nodes.
+ * == Stap 3: Input 1 en eventueel input 2 toekennen per node
+ * 
+ * == Stap 4: Een done list & finished list wordt aangemaakt (leeg maar moeten nodes in kunnen).
+ * Vervolgens wordt A, B & CIN afgehandeld. Hierdoor staat (volgens onze file) 1 & 2 in de "done" list.
+ * Daarna blijft done afgehandeld worden tot alle in de "finished" list staan.
+ * Cout & S zijn dan ingevuld & het proces is voltooid.
+*/
